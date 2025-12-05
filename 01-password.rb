4319b6849e851f47635bbfecad1207ffaa720801
David Cornelius <dev@corneliusconcepts.com>
2025-12-05 05:08:36 -0800

    removed extra comments; turned off logging for large datasets

--- 01-password.rb
+++ 01-password.rb
@@ -1,18 +1,19 @@
 class DialPassword
 
-  attr_accessor :zero_count, :zero_passive
+  attr_accessor :zero_count, :zero_passive, :logit
     
     def initialize
       @zero_count = 0
       @zero_passive = 0
       @curr_pos = 50
       @DIAL_MAX = 99
+      @logit = true
     end
 
   def check_zero_count
     if @curr_pos == 0
       @zero_count += 1
-      puts "ZERO FOUND!"
+      puts "ZERO FOUND!" if @logit
     end
   end
 
@@ -23,7 +24,7 @@ class DialPassword
       @zero_passive += 1 if @curr_pos < 0
       @curr_pos += @DIAL_MAX + 1
     end
-    puts "rotate left #{amount}: #{@curr_pos} (ZeroCount: #{@zero_count}, ZeroPassive: #{@zero_passive})"   
+    puts "rotate left #{amount}: #{@curr_pos} (ZeroCount: #{@zero_count}, ZeroPassive: #{@zero_passive})" if @logit
     check_zero_count
   end
 
@@ -33,7 +34,7 @@ class DialPassword
       @zero_passive += 1 if @curr_pos > (@DIAL_MAX + 1)
       @curr_pos -= @DIAL_MAX + 1
     end
-    puts "rotate right #{amount}: #{@curr_pos} (ZeroCount: #{@zero_count}, ZeroPassive: #{@zero_passive})"
+    puts "rotate right #{amount}: #{@curr_pos} (ZeroCount: #{@zero_count}, ZeroPassive: #{@zero_passive})" if @logit
     check_zero_count
   end
 
@@ -41,18 +42,14 @@ end
 
 dp = DialPassword.new
 data = File.read("input01.txt").split("\n")
-puts "No data." if data.empty?
-data.each do |elem| 
-  if elem[0] == 'L'
-    dp.rotate_left(elem[1,5].to_i)
-  elsif elem[0] == 'R'
-    dp.rotate_right(elem[1,5].to_i)
+dp.logit = false if data.length > 100
+unless data.empty?
+  data.each do |elem| 
+    if elem[0] == 'L'
+      dp.rotate_left(elem[1,5].to_i)
+    elsif elem[0] == 'R'
+      dp.rotate_right(elem[1,5].to_i)
+    end
   end
+  puts "Number of zero positions encountered: directly = #{dp.zero_count}, passively = #{dp.zero_passive}; total = #{dp.zero_count + dp.zero_passive}"
 end
-puts "Number of zero positions encountered: directly = #{dp.zero_count}, passively = #{dp.zero_passive}; total = #{dp.zero_count + dp.zero_passive}"
-
-# day one = 1092
-# day two: 5076 is too low, 7683 is too high
-
-# 50 + 94 = 144; 144 - 99 = 45 (1)
-# 45 + 288 = 333; 333 - 99 = 234 (1); 234 - 99 = 135 (1); 135 - 99 = 36
